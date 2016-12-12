c6f7dd1ba2c8b01fae9c38415501e94c523c3d0a
skipper3 <1337234931@qq.com>
2016-12-12 15:56:34 +0800

    Add files via upload

---
+++ get_features.m
@@ -0,0 +1,49 @@
+% GET_FEATURES: Extracting hierachical convolutional features
+
+function feat = get_features(im, cos_window, layers)
+
+global net
+global enableGPU
+
+if isempty(net)
+    initial_net();
+end
+
+sz_window = size(cos_window);
+
+% Preprocessing
+ img = single(im);        % note: [0, 255] range
+img = imResample(img, net.normalization.imageSize(1:2));
+img = img - net.normalization.averageImage;
+if enableGPU, img = gpuArray(img); end
+
+% edgefeat=Createimage(single(rgb2gray(im)));
+% [x1,y1,z1]=size(edgefeat);
+
+% Run the CNN
+res = vl_simplenn(net,img);
+
+% Initialize feature maps
+feat = cell(length(layers), 1);
+
+for ii = 1:length(layers)
+    % Resize to sz_window
+    if enableGPU
+        x = gather(res(layers(ii)).x); 
+    else
+        x = res(layers(ii)).x;
+    end
+    
+    x = imResample(x, sz_window(1:2));
+    
+    % windowing technique
+    if ~isempty(cos_window),
+        x = bsxfun(@times, x, cos_window);
+    end
+    
+    
+    
+    feat{ii}=x;
+end
+
+end
